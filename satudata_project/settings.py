3ec943db316d5d35ecf6a299aae5b1255e72fb26
Ahmad Zaenun Faiz <zaenun.faiz@gmail.com>
2022-12-16 12:46:49 +0000

    Update Leaflet Static

--- satudata_project/settings.py
+++ satudata_project/settings.py
@@ -142,6 +142,8 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'site/public/static')
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'site/public/static/'),
 )
