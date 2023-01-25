a769a8bfc6274020d1d4574a96b757f0ca337bbb
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-01-25 15:38:22 +0700

    Update web entry

--- operation/ops_models/webentry.py
+++ operation/ops_models/webentry.py
@@ -9,11 +9,24 @@ class WebEntry(models.Model):
   action = models.CharField(max_length=50, editable=False)
   ip = models.GenericIPAddressField(null=True, editable=False)
   timestamp = models.DateTimeField(auto_now_add=True, editable=False)
+  electronic = models.CharField(max_length=50, editable=False)
+  is_touchscreen = models.BooleanField(editable=False)
+  is_bot = models.BooleanField(editable=False)
+  os_type = models.CharField(max_length=50, editable=False)
+  os_version = models.CharField(max_length=50, editable=False)
+  browser_type = models.CharField(max_length=50, editable=False)
+  browser_version = models.CharField(max_length=50, editable=False)
+  device_type = models.CharField(max_length=50, editable=False)
+  device_brand = models.CharField(max_length=50, editable=False)
+  device_model = models.CharField(max_length=50, editable=False)
   username = models.CharField(max_length=50, editable=False)
 
 # WEB ENTRY ADMIN
 @admin.register(WebEntry)
 class WebEntryModel(admin.ModelAdmin):
   search_fields = ('username',)
-  list_display = ('id', 'ip', 'action', 'timestamp', 'username')
-  list_filter = ('ip', 'action', 'timestamp', 'username')
+  list_display = ('id', 'ip', 'electronic', 'browser_type', 'action', 'timestamp', 'username')
+  list_filter = (
+    'ip', 'action', 'timestamp', 'electronic', 
+    'os_type', 'os_version', 'browser_type', 'browser_version', 
+    'device_type', 'device_brand', 'device_model', 'username')
--- operation/signals.py
+++ operation/signals.py
@@ -4,23 +4,137 @@ from operation.ops_models.webentry import WebEntry
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
+  # LOGIKA UNTUK JENIS ELEKTRONIK
+  if request.user_agent.is_mobile:
+    electronic = 'Smartphone'
+  elif request.user_agent.is_tablet:
+    electronic = 'Tablet'
+  elif request.user_agent.is_pc:
+    electronic = 'PC/Laptop'
+  else:
+    electronic = None
+
+  # LOGIKA UNTUK TIPE DEVICE
+  if request.user_agent.device.family == None:
+    device_type = 'None'
+  else:
+    device_type=request.user_agent.device.family
+
+  # LOGIKA UNTUK BRAND DEVICE 
+  if request.user_agent.device.brand == None:
+    device_brand = 'None'
+  else:
+    device_brand=request.user_agent.device.brand
+
+  # LOGIKA UNITUK MODEL DEVICE
+  if request.user_agent.device.model == None:
+    device_model = 'None'
+  else:
+    device_model=request.user_agent.device.model
+
   WebEntry.objects.create(
     action='User Login', 
     ip=request.META.get('REMOTE_ADDR'),
+    electronic=electronic,
+    is_touchscreen=request.user_agent.is_touch_capable,
+    is_bot=request.user_agent.is_bot,
+    os_type=request.user_agent.os.family,
+    os_version=request.user_agent.os.version_string,
+    browser_type=request.user_agent.browser.family,
+    browser_version=request.user_agent.browser.version_string,
+    device_type=device_type,
+    device_brand=device_brand,
+    device_model=device_model,
     username=user.username)
 
 @receiver(user_login_failed)
 def log_user_login_failed(sender, credentials, request, **kwargs):
   creds = credentials
+  
+  # LOGIKA UNTUK JENIS ELEKTRONIK
+  if request.user_agent.is_mobile:
+    electronic = 'Smartphone'
+  elif request.user_agent.is_tablet:
+    electronic = 'Tablet'
+  elif request.user_agent.is_pc:
+    electronic = 'PC/Laptop'
+  else:
+    electronic = None
+
+  # LOGIKA UNTUK TIPE DEVICE
+  if request.user_agent.device.family == None:
+    device_type = 'None'
+  else:
+    device_type=request.user_agent.device.family
+
+  # LOGIKA UNTUK BRAND DEVICE 
+  if request.user_agent.device.brand == None:
+    device_brand = 'None'
+  else:
+    device_brand=request.user_agent.device.brand
+
+  # LOGIKA UNITUK MODEL DEVICE
+  if request.user_agent.device.model == None:
+    device_model = 'None'
+  else:
+    device_model=request.user_agent.device.model
+
   WebEntry.objects.create(
     action='User Login Failed', 
     ip=request.META.get('REMOTE_ADDR'),
+    electronic=electronic,
+    is_touchscreen=request.user_agent.is_touch_capable,
+    is_bot=request.user_agent.is_bot,
+    os_type=request.user_agent.os.family,
+    os_version=request.user_agent.os.version_string,
+    browser_type=request.user_agent.browser.family,
+    browser_version=request.user_agent.browser.version_string,
+    device_type=device_type,
+    device_brand=device_brand,
+    device_model=device_model,
     username=creds['username'])
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
+  # LOGIKA UNTUK JENIS ELEKTRONIK
+  if request.user_agent.is_mobile:
+    electronic = 'Smartphone'
+  elif request.user_agent.is_tablet:
+    electronic = 'Tablet'
+  elif request.user_agent.is_pc:
+    electronic = 'PC/Laptop'
+  else:
+    electronic = None
+
+  # LOGIKA UNTUK TIPE DEVICE
+  if request.user_agent.device.family == None:
+    device_type = 'None'
+  else:
+    device_type=request.user_agent.device.family
+
+  # LOGIKA UNTUK BRAND DEVICE 
+  if request.user_agent.device.brand == None:
+    device_brand = 'None'
+  else:
+    device_brand=request.user_agent.device.brand
+
+  # LOGIKA UNITUK MODEL DEVICE
+  if request.user_agent.device.model == None:
+    device_model = 'None'
+  else:
+    device_model=request.user_agent.device.model
+
   WebEntry.objects.create(
     action='User Logout', 
     ip=request.META.get('REMOTE_ADDR'),
+    electronic=electronic,
+    is_touchscreen=request.user_agent.is_touch_capable,
+    is_bot=request.user_agent.is_bot,
+    os_type=request.user_agent.os.family,
+    os_version=request.user_agent.os.version_string,
+    browser_type=request.user_agent.browser.family,
+    browser_version=request.user_agent.browser.version_string,
+    device_type=device_type,
+    device_brand=device_brand,
+    device_model=device_model,
     username=user.username)
-
