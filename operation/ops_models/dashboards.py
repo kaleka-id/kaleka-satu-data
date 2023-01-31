26feb37b1c1af888123c9fb60f5e586b7d1f1d74
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-01-31 13:57:55 +0700

    Update dashboard

--- operation/ops_models/dashboards.py
+++ operation/ops_models/dashboards.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django_better_admin_arrayfield.models.fields import ArrayField
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
@@ -7,15 +8,15 @@ from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 class Dashboard(models.Model):
   nama = models.CharField(max_length=40)
   category = models.CharField(max_length=20, choices=[('Static CSV', 'Static CSV'), ('Google Sheets', 'Google Sheets'), ('Google BigQuery', 'Google BigQuery'), ('Amazon Redshift', 'Amazon Redshift'), ('PostgreSQL Database', 'PostgreSQL Database'), ('MySQL Database', 'MySQL Database')])
-  email_maintainer = models.CharField(max_length=40)
+  email_maintainers = ArrayField(models.EmailField(), blank=True, null=True)
   page_url = models.CharField(max_length=80, verbose_name='Page URL Path')
   embed_url = models.URLField(max_length=100, verbose_name='Embed URL Path')
   width = models.CharField(max_length=8, help_text="Gunakan CSS unit seperti '%', 'cm', 'mm', 'in', 'px', 'pt', dan 'pc'. Contoh: 500px")
   height = models.CharField(max_length=8, help_text="Gunakan CSS unit seperti '%', 'cm', 'mm', 'in', 'px', 'pt', dan 'pc'. Contoh: 500px")
-  perms_view = ArrayField(models.CharField(max_length=30), null=True, blank=True)
+  permission_view = models.ForeignKey(to=Group, on_delete=models.CASCADE, null=True)
 
 # DASHBOARDS ADMIN
 @admin.register(Dashboard)
 class DashboardModel(admin.ModelAdmin, DynamicArrayMixin):
   search_fields = ('nama',)
-  list_display = ('id', 'nama', 'category', 'email_maintainer', 'perms_view')
+  list_display = ('id', 'nama', 'category', 'email_maintainers', 'permission_view')
