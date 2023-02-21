cead571bb18179c8eee93ac8c9d3f3c45d8ba415
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-02-21 09:44:39 +0700

    Update daashboard

--- operation/ops_models/dashboards.py
+++ operation/ops_models/dashboards.py
@@ -9,6 +9,8 @@ class Dashboard(models.Model):
   nama = models.CharField(max_length=40)
   category = models.CharField(max_length=20, choices=[('Static CSV', 'Static CSV'), ('Google Sheets', 'Google Sheets'), ('Google BigQuery', 'Google BigQuery'), ('Amazon Redshift', 'Amazon Redshift'), ('PostgreSQL Database', 'PostgreSQL Database'), ('MySQL Database', 'MySQL Database')])
   email_maintainers = ArrayField(models.EmailField(), blank=True, null=True)
+  nilai_refresh_data = models.PositiveSmallIntegerField()
+  satuan_refresh_data = models.CharField(max_length=20, choices=[('Realtime', 'Realtime'), ('Menit', 'Menit'), ('Jam', 'Jam'), ('Hari', 'Hari'), ('Minggu', 'Minggu'), ('Bulan', 'Bulan')])
   page_url = models.CharField(max_length=80, verbose_name='Page URL Path')
   embed_url = models.URLField(max_length=100, verbose_name='Embed URL Path')
   width = models.CharField(max_length=8, help_text="Gunakan CSS unit seperti '%', 'cm', 'mm', 'in', 'px', 'pt', dan 'pc'. Contoh: 500px")
@@ -19,4 +21,4 @@ class Dashboard(models.Model):
 @admin.register(Dashboard)
 class DashboardModel(admin.ModelAdmin, DynamicArrayMixin):
   search_fields = ('nama',)
-  list_display = ('id', 'nama', 'category', 'email_maintainers', 'permission_view')
+  list_display = ('id', 'nama', 'category', 'email_maintainers', 'nilai_refresh_data', 'satuan_refresh_data', 'permission_view')
