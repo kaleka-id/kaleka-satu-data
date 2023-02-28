1b10b35d634a7c93901c715f0df0acaee75ab830
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-02-28 09:21:08 +0700

    Update Dictionary

--- operation/ops_models/docs.py
+++ operation/ops_models/docs.py
@@ -2,6 +2,7 @@ from django import forms
 from django.db import models
 from django.contrib import admin
 from django.contrib.postgres.fields import HStoreField
+from django_better_admin_arrayfield.models.fields import ArrayField
 from django_admin_hstore_widget.forms import HStoreFormField
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 import uuid
@@ -21,6 +22,8 @@ class Docs(models.Model):
   gambar = models.FileField(upload_to=get_filename)
   deskripsi = models.TextField()
   dictionary_data = HStoreField(blank=True, null=True, verbose_name='Dictionary')
+  nama_kolom = ArrayField(models.CharField(max_length=30), null=True, blank=True)
+  deskripsi_kolom = ArrayField(models.TextField(max_length=30), null=True, blank=True)
   updated_at = models.DateTimeField(auto_now=True)
 
 # DOCS FORM
@@ -28,13 +31,18 @@ class DocsModelForm(forms.ModelForm):
   dictionary_data = HStoreFormField()
   
   class Meta:
-      model = Docs
-      exclude = ()
+    model = Docs
+    exclude = ()
 
 # DOCS ADMIN
 @admin.register(Docs)
 class DocsModel(admin.ModelAdmin, DynamicArrayMixin):
   search_fields = ('judul',)
   list_display = ('id', 'judul', 'deskripsi')
+
+  fieldsets = [
+    ('Menu', {'fields': ['judul', 'gambar', 'deskripsi']}),
+    ('Tabel', {'fields': ['dictionary_data', 'nama_kolom', 'deskripsi_kolom']}),
+  ]
 
   form = DocsModelForm
