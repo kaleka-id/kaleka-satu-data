6061d8465353c1637ea6ae9b8e32e6421f443dd3
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2023-03-03 10:12:42 +0700

    Update Keterangan Wilayah Desa

--- data/dataset/wilayah_desa.py
+++ data/dataset/wilayah_desa.py
@@ -17,6 +17,7 @@ class WilayahDesa(models.Model):
   geom = models.MultiPolygonField(verbose_name='Lokasi', db_index=True)
   dasar_hukum = models.CharField(max_length=120)
   status_data = models.CharField(max_length=11, verbose_name='Status Data', choices=[('Updated', 'Updated'), ('Depreciated', 'Depreciated')])
+  keterangan = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
